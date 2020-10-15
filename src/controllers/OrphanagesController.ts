112058c5b5776ab4899967efc4287dc9e55110c2
Ari Jesus <arimario.jesus@hotmail.com>
2020-10-15 02:56:52 -0300

    Finish Day 03

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -55,7 +55,7 @@ export default {
             about,
             instructions,
             opening_hours,
-            open_on_weekends,
+            open_on_weekends: open_on_weekends === 'true',
             images
         };
 
@@ -75,7 +75,7 @@ export default {
         });
 
         await schema.validate(data, {
-            abortEarly: false,
+            abortEarly: false
         });
     
         const orphanage = orphanagesRepository.create(data);
